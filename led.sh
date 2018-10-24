7bdb4ec3a01d6173ff1d6ffc8f07b214c5405c90
Georgiy Sitnikov <GAS85@users.noreply.github.com>
2018-10-24 10:39:00 +0200

    Update led.sh

--- led.sh
+++ led.sh
@@ -39,13 +39,15 @@ green=$LEDs/green:ph07:led4
 ####################################
 
 #Check if folder exist
-if [ -d "$LEDs" ]; then
+if [[ ! -d "$LEDs" ]]; then
 
 	echo "ERROR - LED Folder not found under $LEDs"
 
 	exit 1
 
-	if [ ! -f "$blue" ]; then
+else
+    
+	if [[ ! -f "$blue/brightness" ]]; then
 
 		echo "ERROR - LEDs not found under e.g. $blue"
 
@@ -60,30 +62,85 @@ fi
 #none battery-charging-or-full battery-charging battery-full battery-charging-blink-full-solid ac-online usb-online mmc0 mmc1 timer disk-activity heartbeat backlight cpu0 cpu1 default-on rfkill0 rfkill1 rfkill2 rfkill4 
 #echo TRIGGER > $blue/trigger
 
-if [ -z "$triggerB" ]; echo $triggerB > $blau/trigger
-if [ -z "$triggerO" ]; echo $triggerO > $orange/trigger
-if [ -z "$triggerW" ]; echo $triggerW > $white/trigger
-if [ -z "$triggerG" ]; echo $triggerG > $green/trigger
+if [[ $triggerB ]]; then
+
+	echo $triggerB > $blau/trigger
+
+fi
+
+if [[ $triggerO ]]; then
+
+	echo $triggerO > $orange/trigger
+
+fi
+
+if [[ $triggerW ]];  then
+
+	echo $triggerW > $white/trigger
+
+fi
+
+if [[ $triggerG ]];  then
+
+	echo $triggerG > $green/trigger
+
+fi
 
 #set brightness
 ##if Argument "off" to set low brightness, e.g. for night
 
-if [[ $1 == "off" ]]
+if [[ $1 == "off" ]]; then
 
-	then
+	if [[ $triggerB ]]; then
 
-		if [ -z "$triggerB" ]; echo $BrightnessOff > $blau/brightness
-		if [ -z "$triggerO" ]; echo $BrightnessOff > $orange/brightness
-		if [ -z "$triggerW" ]; echo $BrightnessOff > $white/brightness
-		if [ -z "$triggerG" ]; echo $BrightnessOff > $green/brightness
-
-	else
-
-		if [ -z "$triggerB" ]; echo $BrightnessOn > $blau/brightness
-		if [ -z "$triggerO" ]; echo $BrightnessOn > $orange/brightness
-		if [ -z "$triggerW" ]; echo $BrightnessOn > $white/brightness
-		if [ -z "$triggerG" ]; echo $BrightnessOn > $green/brightness
+		echo $BrightnessOff > $blau/brightness
 
 	fi
+
+	if [[ $triggerO ]];  then
+
+		echo $BrightnessOff > $orange/brightness
+
+	fi
+
+	if [[ $triggerW ]];  then
+
+		echo $BrightnessOff > $white/brightness
+
+	fi
+
+	if [[ $triggerG ]];  then
+
+		echo $BrightnessOff > $green/brightness
+
+	fi
+
+else
+
+	if [[ $triggerB ]];  then
+
+		echo $BrightnessOn > $blau/brightness
+
+	fi
+
+	if [[ $triggerO ]];  then
+
+		echo $BrightnessOn > $orange/brightness
+
+	fi
+
+	if [[ $triggerW ]];  then
+
+		echo $BrightnessOn > $white/brightness
+
+	fi
+
+	if [[ $triggerG ]]; then
+
+		echo $BrightnessOn > $green/brightness
+
+	fi
+
+fi
 
 exit 0
