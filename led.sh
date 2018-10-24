8379807d27eb17dedfa8a63d591627094408da7d
Georgiy Sitnikov <GAS85@users.noreply.github.com>
2018-10-24 09:37:41 +0200

    Update led.sh
    corrections added
    added check if folder set correctly

--- led.sh
+++ led.sh
@@ -5,16 +5,32 @@
 
 ### Configuration start ###
 
+#path to your LEDs
 LEDs=/sys/class/leds/
-blue=$LEDs/blue:ph21:led1
-orange=$LEDs/orange:ph20:led2
-white=$LEDs/white:ph11:led3
-green=$LEDs/green:ph07:led4
 
 #Brightness for "day"
 BrightnessOn=50
 #Brightness for "night"
 BrightnessOff=0
+
+#set trigger
+#possible triggers: 
+#none battery-charging-or-full battery-charging battery-full battery-charging-blink-full-solid ac-online usb-online mmc0 mmc1 timer disk-activity heartbeat backlight cpu0 cpu1 default-on rfkill0 rfkill1 rfkill2 rfkill4 
+#Just delete value of trigger and it will not be touched (e.g. as green)
+	#for blue LED
+	triggerB=disk-activity
+	#for orange LED
+	triggerO=cpu0
+	#for white LED
+	triggerW=cpu1
+	#for green LED
+	triggerG=
+
+#Usually not need to be edited. Path to different LEDs.
+blue=$LEDs/blue:ph21:led1
+orange=$LEDs/orange:ph20:led2
+white=$LEDs/white:ph11:led3
+green=$LEDs/green:ph07:led4
 
 ### Configuration end ###
 
@@ -22,30 +38,47 @@ BrightnessOff=0
 ### DO NOT TOUCH AFTER THIS LINE ###
 ####################################
 
+#Check if folder exist
+if [ -d "$SAMBA" ]; then
+
+	echo "ERROR - LED Folder not found under $LEDs"
+
+	exit 1
+
+	if [ ! -f "$blue" ]; then
+
+		echo "ERROR - LEDs not found under e.g. $blue"
+
+		exit 1
+
+	fi
+
+fi
+
 #set trigger
 #possible triggers: 
 #none battery-charging-or-full battery-charging battery-full battery-charging-blink-full-solid ac-online usb-online mmc0 mmc1 timer disk-activity heartbeat backlight cpu0 cpu1 default-on rfkill0 rfkill1 rfkill2 rfkill4 
 #echo TRIGGER > $blue/trigger
 
-echo disk-activity > $blau/trigger
-echo cpu0 > $orange/trigger
-echo cpu1 > $white/trigger
-#echo mmc0 > $green/trigger
+echo $triggerB > $blau/trigger
+echo $triggerO > $orange/trigger
+echo $triggerW > $white/trigger
+echo triggerG > $green/trigger
 
 #set brightness
 ##if Argument "off" to set low brightness, e.g. for night
 
 if [[ $1 == "off" ]]
   then
-	echo BrightnessOn=50 > $blau/brightness
-	echo BrightnessOn=50 > $orange/brightness
-	echo BrightnessOn=50 > $white/brightness
-#	echo BrightnessOn=50 > $green/brightness
+	echo BrightnessOff > $blau/brightness
+	echo BrightnessOff > $orange/brightness
+	echo BrightnessOff > $white/brightness
+	echo BrightnessOff > $green/brightness
   else
 	echo $BrightnessOn > $blau/brightness
 	echo $BrightnessOn > $orange/brightness
 	echo $BrightnessOn > $white/brightness
-#	echo $BrightnessOn > $green/brightness
+	echo $BrightnessOn > $green/brightness
 fi
 
 exit 0
